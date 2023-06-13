af3da8c4947d0b24a12b4dbb880c8ecd2d8b9dbd
ReyKiptoo <kiptoorey@gmail.com>
2023-06-13 21:20:02 +0300

    no script

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-window.addEventListener("load", function () {
-  // Set a timeout to hide the URL bar after the page has loaded
-  setTimeout(function () {
-    window.scrollTo(0, 1); // Scroll to hide the URL bar
-  }, 0);
-});
+// window.addEventListener("load", function () {
+//   // Set a timeout to hide the URL bar after the page has loaded
+//   setTimeout(function () {
+//     window.scrollTo(0, 1); // Scroll to hide the URL bar
+//   }, 0);
+// });
