569ec441b0294c48f362ca327fa8a4ca41bbc793
ReyKiptoo <kiptoorey@gmail.com>
2023-06-14 11:50:13 +0300

    play pause switch

--- script.js
+++ script.js
@@ -3,8 +3,11 @@
 const playStop = document.querySelector(".fa-play");
 const body = document.querySelector("body");
 
+console.log(playStop);
 playStop.addEventListener("click", function () {
   console.log("Play Clicked");
   playStop.classList.remove("fa-play");
-  playStop.classList.remove("fa-pause");
+  console.log(playStop);
+  playStop.classList.add("fa-pause");
+  console.log(playStop);
 });
