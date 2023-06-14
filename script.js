9cd3e462e7c541d44c2f07301eb465114596ed4a
ReyKiptoo <kiptoorey@gmail.com>
2023-06-14 19:07:45 +0300

    toggling board border

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 // Getting elements
 
 const playStop = document.querySelector(".fa-play");
+const boardPiecesAndCoords = document.querySelector(".fa-chess");
 const body = document.querySelector("body");
+const board = document.querySelector(".board");
 
 let playing = true;
+let piecesShowing = false;
 
-console.log(playStop);
 playStop.addEventListener("click", function () {
+  changeIcon();
+});
+
+boardPiecesAndCoords.addEventListener("click", togglePiecesAndCoordinates);
+
+function changeIcon() {
   if (playing) {
     playing = false;
     playStop.classList.remove("fa-play");
@@ -16,4 +24,14 @@ playStop.addEventListener("click", function () {
     playStop.classList.add("fa-play");
     playStop.classList.remove("fa-pause");
   }
-});
+}
+
+function togglePiecesAndCoordinates() {
+  if (!piecesShowing) {
+    piecesShowing = true;
+    board.style.border = "2px solid #ffffff";
+  } else if (piecesShowing) {
+    piecesShowing = false;
+    board.style.border = "2px solid #ffd585";
+  }
+}
