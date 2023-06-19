388b6fe2bca0274b2029f2059e8f67e949aa0856
ReyKiptoo <kiptoorey@gmail.com>
2023-06-19 20:30:56 +0300

    first javascript changes

--- script.js
+++ script.js
@@ -18,6 +18,29 @@ const blackWrapper = document.querySelector(".blackWrapper");
 const topPieces = document.querySelectorAll(".board .fa-solid");
 const bottomPieces = document.querySelectorAll(".board .fa-regular");
 
+const array1 = ["a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1"];
+const array2 = ["a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2"];
+const array3 = ["a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3"];
+const array4 = ["a4", "b4", "c4", "d4", "e4", "f4", "g4", "h4"];
+const array5 = ["a5", "b5", "c5", "d5", "e5", "f5", "g5", "h5"];
+const array6 = ["a6", "b6", "c6", "d6", "e6", "f6", "g6", "h6"];
+const array7 = ["a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7"];
+const array8 = ["a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8"];
+
+const fullCoordsArray = [
+  ...array1,
+  ...array2,
+  ...array3,
+  ...array4,
+  ...array5,
+  ...array6,
+  ...array7,
+  ...array8,
+];
+
+const randomCoordinate = fullCoordsArray[randomIntFromInterval(0, 63)];
+
+console.log(randomCoordinate);
 let playing = true;
 let piecesShowing = false;
 let coordsShowing = false;
@@ -113,4 +136,10 @@ function setPlayUIToWhite() {
     piece.classList.remove("fa-solid");
     piece.classList.add("fa-regular");
   }
+}
+
+// Function to generate random number between two numbers
+function randomIntFromInterval(min, max) {
+  // min and max included
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
